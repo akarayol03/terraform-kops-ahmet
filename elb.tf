0b6262707a11ec376d2de7724c6cadb85b4347e1
ahmet karayol <37154452+akarayol03@users.noreply.github.com>
2020-02-11 00:46:01 -0600

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 
-resource "aws_elb" "api-acirrustech-com" {
-  name = "api-acirrustech-com-ai4crq"
+resource "aws_elb" "api-originalpeak-com" {
+  name = "api-originalpeak-com-ai4crq"
 
   listener = {
     instance_port     = 443
@@ -9,8 +9,8 @@ resource "aws_elb" "api-acirrustech-com" {
     lb_protocol       = "TCP"
   }
 
-  security_groups = ["${aws_security_group.api-elb-acirrustech-com.id}"]
-  subnets         = ["${aws_subnet.utility-eu-west-1a-acirrustech-com.id}", "${aws_subnet.utility-eu-west-1b-acirrustech-com.id}", "${aws_subnet.utility-eu-west-1c-acirrustech-com.id}"]
+  security_groups = ["${aws_security_group.api-elb-originalpeak-com.id}"]
+  subnets         = ["${aws_subnet.utility-eu-west-1a-originalpeak-com.id}", "${aws_subnet.utility-eu-west-1b-originalpeak-com.id}", "${aws_subnet.utility-eu-west-1c-acirrustech-com.id}"]
 
   health_check = {
     target              = "SSL:443"
@@ -24,14 +24,14 @@ resource "aws_elb" "api-acirrustech-com" {
   idle_timeout              = 300
 
   tags = {
-    KubernetesCluster                       = "acirrustech.com"
-    Name                                    = "api.acirrustech.com"
-    "kubernetes.io/cluster/acirrustech.com" = "owned"
+    KubernetesCluster                       = "originalpeak.com."
+    Name                                    = "api.originalpeak.com."
+    "kubernetes.io/cluster/originalpeak.com." = "owned"
   }
 }
 
-resource "aws_elb" "bastion-acirrustech-com" {
-  name = "bastion-acirrustech-com-1fi9s4"
+resource "aws_elb" "bastion-originalpeak-com" {
+  name = "bastion-originalpeak-com-1fi9s4"
 
   listener = {
     instance_port     = 22
@@ -40,8 +40,8 @@ resource "aws_elb" "bastion-acirrustech-com" {
     lb_protocol       = "TCP"
   }
 
-  security_groups = ["${aws_security_group.bastion-elb-acirrustech-com.id}"]
-  subnets         = ["${aws_subnet.utility-eu-west-1a-acirrustech-com.id}", "${aws_subnet.utility-eu-west-1b-acirrustech-com.id}", "${aws_subnet.utility-eu-west-1c-acirrustech-com.id}"]
+  security_groups = ["${aws_security_group.bastion-elb-originalpeak-com.id}"]
+  subnets         = ["${aws_subnet.utility-eu-west-1a-originalpeak-com.id}", "${aws_subnet.utility-eu-west-1b-originalpeak-com.id}", "${aws_subnet.utility-eu-west-1c-acirrustech-com.id}"]
 
   health_check = {
     target              = "TCP:22"
@@ -54,8 +54,8 @@ resource "aws_elb" "bastion-acirrustech-com" {
   idle_timeout = 300
 
   tags = {
-    KubernetesCluster                       = "acirrustech.com"
-    Name                                    = "bastion.acirrustech.com"
-    "kubernetes.io/cluster/acirrustech.com" = "owned"
+    KubernetesCluster                       = "originalpeak.com."
+    Name                                    = "bastion.originalpeak.com."
+    "kubernetes.io/cluster/originalpeak.com." = "owned"
   }
 }
